b3ff1e52def351f1f3a01afffaf2ef918a61e86f
yourusername <oko23001@byui.edu>
2025-07-05 23:27:41 +0100

    git commit "Completed Exercise2"

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentge:  ");
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Print the letter grade
+        Console.WriteLine($"Your grade is: {letter}");
+
+        // Check if the student passed
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep working at it! You'll do better next time.");
+        }
     }
 }
+    
