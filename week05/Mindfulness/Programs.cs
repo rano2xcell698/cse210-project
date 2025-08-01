2e44e336dbe1a2c0e3e83b0c9fedfa4eb97cfdd0
yourusername <oko23001@byui.edu>
2025-08-01 13:55:29 +0100

    Added Visualization Activity

--- week05/Mindfulness/Programs.cs
+++ week05/Mindfulness/Programs.cs
@@ -7,11 +7,12 @@ class Program
         while (true)
         {
             Console.Clear();
-            Console.WriteLine("Mindfulness Program");
+            Console.WriteLine("=== Mindfulness Program ===");
             Console.WriteLine("1. Breathing Activity");
             Console.WriteLine("2. Reflection Activity");
             Console.WriteLine("3. Listing Activity");
-            Console.WriteLine("4. Quit");
+            Console.WriteLine("4. Visualization Activity");
+            Console.WriteLine("5. Quit");
             Console.Write("Choose an option: ");
             string choice = Console.ReadLine();
 
@@ -29,15 +30,20 @@ class Program
                     activity = new ListingActivity();
                     break;
                 case "4":
-                    Console.WriteLine("Goodbye!");
+                    activity = new VisualizationActivity();
+                    break;
+                case "5":
+                    Console.WriteLine("\nThank you for using the Mindfulness Program. Goodbye!");
                     return;
                 default:
-                    Console.WriteLine("Invalid input. Try again.");
-                    Thread.Sleep(1000);
+                    Console.WriteLine("Invalid input. Press Enter to try again.");
+                    Console.ReadLine();
                     continue;
             }
 
             activity.Run();
+            Console.WriteLine("\nPress Enter to return to the main menu...");
+            Console.ReadLine();
         }
     }
 }
